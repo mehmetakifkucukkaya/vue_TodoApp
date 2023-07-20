2ec6f57544f3ba0cc58142c34eb46922946909d3
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-07-20 15:20:25 +0300

    Header ve AddTaskButton componentları oluşturuldu

---
+++ src/components/AddTaskButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onClick()" class="btn">{{ text }}</button>
+</template>
+
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+  },
+  methods: {
+    onClick() {
+      // Butona tıklandığında çalışacak metot.
+      console.log("clicked");
+    },
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <AddTaskButton text="Görev Ekle" />
+  </header>
+</template>
+
+
+<script>
+import AddTaskButton from "./AddTaskButton.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    AddTaskButton,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  margin-bottom: 22px;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
